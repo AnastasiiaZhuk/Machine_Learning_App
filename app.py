40654ea420a1463d6cbcbe717ba47bdf6c2aab30
AnastasiiaZhuk <n.forest111@gmail.com>
2020-07-18 13:46:33 +0300

    Small changes in code structure in app.py  On branch develop  Changes to be committed: 	new file:   .gitignore 	modified:   app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import random
 from abc import ABCMeta, abstractmethod
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier as RandomForestClassifier_
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
@@ -15,10 +15,10 @@ from sklearn.metrics import precision_score, recall_score
 @st.cache(persist=True)
 def load_data():
     data = pd.read_csv("/home/nastiositi/disk_e/PycharmProjects/Machine_Learning_App/mushrooms.csv")
-    labelencoder = LabelEncoder()
+    label_encoder = LabelEncoder()
 
     for col in data.columns:
-        data[col] = labelencoder.fit_transform(data[col])
+        data[col] = label_encoder.fit_transform(data[col])
     return data
 
 
@@ -38,43 +38,50 @@ class Classifier(metaclass=ABCMeta):
 
 class LogisticClassifier(Classifier):
 
-    def classify(self, C, max_iter, penalty='l2'):
-        st.subheader("Logistic Regression Results")
-        model = LogisticRegression(C=C,
-                                   penalty='l2',
+    def classify(self, penalty='l2'):
+        c = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
+        max_iter = st.sidebar.slider("Maximum number of iterations", 200, 1000, key='max_iter')
+        model = LogisticRegression(C=c,
+                                   penalty=penalty,
                                    max_iter=max_iter)
-        data = pd.DataFrame([C, max_iter, penalty], ['C', 'max_iter', 'penalty'])
+        data = pd.DataFrame([c, max_iter, penalty], ['C', 'max_iter', 'penalty'])
 
         return model, data
 
 
-class RandomFrstClassifier(Classifier):
+class RandomForestClassifier(Classifier):
 
-    def classify(self, n_estimators, max_depth, bootstrap, n_jobs=-1):
-        st.subheader("Random Forest Results")
-        model = RandomForestClassifier(n_estimators=n_estimators,
-                                       max_depth=max_depth,
-                                       bootstrap=bootstrap,
-                                       n_jobs=-1)
-        data = pd.DataFrame([n_estimators, max_depth, bootstrap, n_jobs], ['n_estimators', 'max_depth', 'bootstrap', 'n_jobs'])
+    def classify(self, n_jobs=-1):
+        n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10,
+                                               key='n_estimators')
+        max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step=1, key='n_estimators')
+        bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
+        model = RandomForestClassifier_(n_estimators=n_estimators,
+                                        max_depth=max_depth,
+                                        bootstrap=bootstrap,
+                                        n_jobs=-1)
+        data = pd.DataFrame([n_estimators, max_depth, bootstrap, n_jobs],
+                            ['n_estimators', 'max_depth', 'bootstrap', 'n_jobs'])
 
         return model, data
 
 
 class SVMClassifier(Classifier):
 
-    def classify(self, C, kernel, gamma):
-        st.subheader("Support Vector Machine (SVM) Results")
-        model = SVC(C=C,
+    def classify(self):
+        c = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
+        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
+        gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
+
+        model = SVC(C=c,
                     kernel=kernel,
                     gamma=gamma)
-        data = pd.DataFrame([C, kernel, gamma], ['C', 'kernel', 'gamma'])
+        data = pd.DataFrame([c, kernel, gamma], ['C', 'kernel', 'gamma'])
 
         return model, data
 
 
 class ClassifierFactory:
-
     df = load_data()
     class_names = ['edible', 'poisonous']
     x_train, x_test, y_train, y_test = split(df)
@@ -96,30 +103,14 @@ class ClassifierFactory:
             plot_precision_recall_curve(model, cls.x_test, cls.y_test)
             st.pyplot()
 
-    @classmethod
-    def get_classificator(cls, classifier):
-        model = None
-
-        if classifier == 'Support Vector Machine (SVM)':
-            C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
-            kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
-            gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
-            model = SVMClassifier().classify(C, kernel, gamma)
-
-        elif classifier == 'Logistic Regression':
-            C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
-            max_iter = st.sidebar.slider("Maximum number of iterations", 200, 1000, key='max_iter')
-            model = LogisticClassifier().classify(C, max_iter)
-            return model
-
-        elif classifier == 'Random Forest':
-            n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10,
-                                                   key='n_estimators')
-            max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step=1, key='n_estimators')
-            bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
-            model = RandomFrstClassifier().classify(n_estimators, max_depth, bootstrap)
-            return model
-        return model
+    @staticmethod
+    def get_classifier(classifier):
+        switcher = {
+            "Random Forest": RandomForestClassifier,
+            "Logistic Regression": LogisticClassifier,
+            "Support Vector Machine (SVM)": SVMClassifier,
+        }
+        return switcher.get(classifier, ValueError)
 
 
 def main():
@@ -128,28 +119,33 @@ def main():
     st.markdown("Are your mushrooms edible or poisonous? 🍄")
     st.sidebar.markdown("Are your mushrooms edible or poisonous? 🍄")
     st.sidebar.subheader("Choose Classifier")
-    classifier = st.sidebar.selectbox("Classifier",
-                                      ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest"))
-    model, data = ClassifierFactory.get_classificator(classifier)
+    classifiers = ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest")
 
-    metrics = st.sidebar.multiselect("What metrics to plot?",
-                                     ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+    classifier = st.sidebar.selectbox("Classifier",
+                                      classifiers)
+
     factory = ClassifierFactory()
+    class_factory = factory.get_classifier(classifier)
+    model, data = class_factory().classify()
+
+    metric_list = ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve')
+    metrics = st.sidebar.multiselect("What metrics to plot?",
+                                     metric_list)
 
     if st.sidebar.button("Classify", key='classify1'):
-
+        st.subheader(f'{classifier} Results: ')
         model.fit(factory.x_train, factory.y_train)
         data.columns = ['Using these Parameters']
         st.table(data)
         accuracy = model.score(factory.x_test, factory.y_test)
         y_pred = model.predict(factory.x_test)
+
         st.write("Accuracy: ", accuracy.round(2))
         st.write("Precision: ", precision_score(factory.y_test, y_pred, labels=factory.class_names).round(2))
         st.write("Recall: ", recall_score(factory.y_test, y_pred, labels=factory.class_names).round(2))
         factory.plot_metrics(metrics, model)
 
     if st.sidebar.checkbox("Show raw data", False):
-
         st.subheader("Mushroom Data Set (Classification)")
         rand1 = random.randrange(1, 10, 1)
         rand2 = random.randint(1, 8100)
